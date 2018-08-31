fff38c2a83aa4f24ca474666f3dd944c567a22de
vonjy <ntsoanyaina@gmail.com>
2018-08-31 09:54:27 +0400

    Backup list Pokemon

--- pokemon-app/app/app.component.js
+++ pokemon-app/app/app.component.js
@@ -6,15 +6,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var mock_pokemons_1 = require("./mock-pokemons");
 var AppComponent = (function () {
     function AppComponent() {
+        this.pokemons = null;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.pokemons = mock_pokemons_1.POKEMONS;
+    };
+    AppComponent.prototype.selectPokemon = function (pokemon) {
+        console.log('Vous avez selectionné ' + pokemon.name);
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pokemon-app',
-        template: "<h1>Hello, Angular 2 !</h1>",
+        template: "<h1 class='center'>Pok\u00E9mons</h1>\n  <div class='container'>\n  <div class=\"row\">\n  <div *ngFor='let pokemon of pokemons' class=\"col s6 m4\">\n\t<div class=\"card horizontal\" (click)=\"selectPokemon(pokemon)\">\n\t  <div class=\"card-image\">\n\t\t<img [src]=\"pokemon.picture\">\n\t  </div>\n\t  <div class=\"card-stacked\">\n\t\t<div class=\"card-content\">\n\t\t  <p>{{ pokemon.name }}</p>\n\t\t  <p><small>{{ pokemon.created }}</small></p>\n\t\t</div>\n\t  </div>\n\t</div>\n  </div>\n  </div>\n  </div>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- pokemon-app/app/app.component.ts
+++ pokemon-app/app/app.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
-	selector: 'pokemon-app',
-	template: `<h1>Hello, Angular 2 !</h1>`,
+  selector: 'pokemon-app',
+  template: `<h1 class='center'>Pokémons</h1>
+  <div class='container'>
+  <div class="row">
+  <div *ngFor='let pokemon of pokemons' class="col s6 m4">
+	<div class="card horizontal" (click)="selectPokemon(pokemon)">
+	  <div class="card-image">
+		<img [src]="pokemon.picture">
+	  </div>
+	  <div class="card-stacked">
+		<div class="card-content">
+		  <p>{{ pokemon.name }}</p>
+		  <p><small>{{ pokemon.created }}</small></p>
+		</div>
+	  </div>
+	</div>
+  </div>
+  </div>
+  </div>`
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  pokemons: Pokemon[] = null;
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez selectionné ' + pokemon.name);
+  }
+  
+}
